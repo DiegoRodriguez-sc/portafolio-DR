154ea95a95a868ad4dbbbdd8dbcfb2d929c87f6d
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-01-31 16:14:51 -0300

    project structure added

---
+++ src/helpers/data.js
@@ -0,0 +1,90 @@
+import noteApp from "../assets/projects/note-app-login.PNG";
+//------------------------------------------------------------------------
+import memorama from "../assets/projects/memorama.PNG";
+//------------------------------------------------------------------------
+import planetApp from "../assets/projects/planet-app.png";
+//------------------------------------------------------------------------
+import ppT from "../assets/projects/piedra-papel-tijera.png";
+//------------------------------------------------------------------------
+import pokedex from "../assets/projects/pokedex.png";
+//------------------------------------------------------------------------
+import countryApp from "../assets/projects/countries-app.png";
+//------------------------------------------------------------------------
+import superHeroApp from "../assets/projects/superheroApp2.PNG";
+//------------------------------------------------------------------------
+import ongWeb from "../assets/projects/ong-app-loading.PNG";
+
+export const data = [
+  {
+    title: "Note App.",
+    img: noteApp,
+    tech: "Js css Bootstrap React node MongoDB",
+    link: "https://note-app-frontend.netlify.app/",
+    repo: "https://github.com/DiegoRodriguez-sc/note-app-frontend",
+    small:"Built using Node, Express, MongoDB, CSS + Bootstrap, React.js and React Router.",
+    p:"This is a full-stack website that you can use to create, edit, and delete picture notes."
+  },
+  {
+    title: "Memorama.",
+    img: memorama,
+    tech: "Js css html",
+    link: "https://diegorodriguez-sc.github.io/memorama/",
+    repo: "https://github.com/DiegoRodriguez-sc/memorama",
+    small:"Built using CSS, Vanilla JavaScript and Html.",
+    p:"This is a website where you can play by memorizing cards and finding pairs."
+  },
+  {
+    title: "Planet App.",
+    img: planetApp,
+    tech: "Js css React",
+    link: "https://planet-app.netlify.app/",
+    repo: "https://github.com/DiegoRodriguez-sc/planet-app",
+    small:"Built using CSS, React.js and React Router",
+    p:"This is a website where you can view information about the planets in our galaxy."
+  },
+  {
+    title: "Piedra Papel Tijera.",
+    img: ppT,
+    tech: "Js css html",
+    link: "https://diegorodriguez-sc.github.io/Rock-Paper-Scissors/",
+    repo: "https://github.com/DiegoRodriguez-sc/Rock-Paper-Scissors",
+    small:"Built using CSS, Vanilla JavaScript and Html.",
+    p:"This is a website where you can play the famous rock, paper or scissors"
+  },
+  {
+    title: "Pokedex.",
+    img: pokedex,
+    tech: "Js css html",
+    link: "https://diegorodriguez-sc.github.io/pokedex/",
+    repo: "https://github.com/DiegoRodriguez-sc/pokedex",
+    small:"Built using CSS, React.js and React Router",
+    p:"This is a website where you can see information about the pokemon"
+  },
+  {
+    title: "Countries App.",
+    img: countryApp,
+    tech: "Js css sass React",
+    link: "",
+    repo: "https://github.com/DiegoRodriguez-sc/countries-app",
+    small:"Built using CSS + Sass, React.js and React Router.",
+    p:"This is a website where you can see information about the countries of the world"
+  },
+  {
+    title: "Super App.",
+    img: superHeroApp,
+    tech: "Js css bootstrap React",
+    link: "",
+    repo: "https://github.com/DiegoRodriguez-sc/super-hero-app",
+    small:"Built using CSS + Bootstrap, React.js and React Router.",
+    p:"This is a website where you can see create your team of superheroes, and see both individual and team statistics"
+  },
+  {
+    title: "web Ong.",
+    img: ongWeb,
+    tech: "Js css Bootstrap React node MongoDB",
+    link: "",
+    repo: "https://github.com/FedeHC/ONG-Somos-Mas",
+    small:"Built using Node, Express, MongoDB, CSS + Bootstrap, React.js and React Router.",
+    p:"This project was developed in React over 6 weeks together with a team of 5 colleagues during the Alkemy Labs acceleration, in order to be job ready by simulating a real work environment together with a mentor."
+  },
+];
--- src/pages/ProjectScreen.js
+++ src/pages/ProjectScreen.js
@@ -1,11 +1,76 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Link,
+  Typography,
+} from "@mui/material";
+import { data } from "../helpers/data";
 
 const ProjectScreen = () => {
- return (
-  <div>
-   
-  </div>
- );
-}
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate("/");
+  };
+
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Box width={"100%"}>
+        <Box mb={10} />
+        <Link
+          color={"#E2E2E2"}
+          style={{ cursor: "pointer", fontSize: "2rem" }}
+          onClick={handleBack}
+        >
+          back
+        </Link>
+        <Box mb={5} />
+        <Typography variant="h3" sx={{ fontWeight: "bold" }}>
+          Mis projectos
+        </Typography>
+        <Box mb={2} />
+        <Typography>Estos son mis projectos.....</Typography>
+        <Box mb={2} />
+        <Box sx={{display:"flex", flexWrap:"wrap", gap:2, justifyContent:"center"}}>
+          {data.map((pr) => (
+            <Card key={pr.title} sx={{ width:{xs:345, md:265}, bgcolor:"#1C1C1C", color:"#E2E2E2"}}>
+              <CardMedia
+                component="img"
+                height="140"
+                image={pr.img}
+                alt={pr.title}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h4" component="div" color={"#E2E2E2"}>
+                  {pr.title}
+                </Typography>
+                <Typography variant="h5" color={"#E2E2E2"}>
+                  {pr.p}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="medium">Repo</Button>
+                <Button size="medium">Demo</Button>
+              </CardActions>
+            </Card>
+          ))}
+        </Box>
+      </Box>
+    </Container>
+  );
+};
 
 export default ProjectScreen;
