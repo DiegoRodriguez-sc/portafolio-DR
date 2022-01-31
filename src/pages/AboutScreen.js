b0de6002856aa94094d841da8a1ccce3c250c0fe
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-01-31 18:45:40 -0300

    about fixed

--- src/pages/AboutScreen.js
+++ src/pages/AboutScreen.js
@@ -45,24 +45,42 @@ const AboutScreen = ({ changeTheme }) => {
         />
         <Box mb={2} />
         <Typography paragraph={true} variant="h4">
-          Mi nombre es Diego Rodriguez,soy estudiante universitario en
-          Universidad Nacional de la Plata. Realmente disfruto resolviendo
-          problemas y haciendo que las cosas sean agradables y fáciles de usar.
-          No puedo dejar de aprender cosas nuevas, cuanto más mejor. También me
-          encantan los videojuegos. Una afición que me llevó a conocer y
-          competir con buena gente. Ah, y dulce de leche ¡Tengo una pasión por
-          el dulce de leche!
+          Mi nombre es Diego Rodriguez, soy desarrollador web y tambien
+          estudiante de Lic. en Sistemas en{" "}
+          <Link
+            color={changeTheme ? "#E2E2E2" : "#050038"}
+            style={{ cursor: "pointer", fontSize: "2rem" }}
+            href="https://www.info.unlp.edu.ar/"
+            target={"_blank"}
+          >
+            La Universidad Nacional de la Plata.
+          </Link>{" "}
+          <br />
+          <br />
+          Realmente disfruto resolviendo problemas y haciendo que las cosas sean
+          agradables y fáciles de usar. No puedo dejar de aprender cosas
+          nuevas!, cuanto más mejor.
+          <br />
+          <br />
+          También me encantan los videojuegos. Una afición que me llevó a
+          conocer y competir con buena gente. Ah, y dulce de leche ¡Tengo una
+          pasión por el dulce de leche!
         </Typography>
         <Typography variant="h3">Que Hago?</Typography>
         <Box mb={2} />
         <Typography paragraph={true} variant="h4">
-          Para el front-end, suelo trabajar con Javascript, ya sea de forma
-          independiente o incluyendo marcos populares como
-          ReactJS,ReactNative(aprendiendo). También embellezco la web usando
-          Sass, CSS y, cuando sea necesario, cualquiera de sus amigos:
-          Bootstrap, Material-UI, etc. Para el back-end también trabajo con
-          Javascript (NodeJS, Express, MongoDB, etc.). Pero últimamente estoy
-          aprendiendo java con Spring Boot.
+          <strong>Soy programador</strong>, Para el front-end, suelo trabajar
+          con Javascript, ya sea de forma independiente o incluyendo marcos
+          populares como ReactJS, ReactNative(aprendiendo).
+          <br />
+          <br />
+          También embellezco la web usando Sass, CSS y, cuando sea necesario,
+          cualquiera de sus amigos: Bootstrap, Material-UI, etc.
+          <br />
+          <br />
+          Para el back-end también trabajo con Javascript (NodeJS, Express),
+          pero últimamente estoy aprendiendo java con Spring Boot. En base de
+          datos utilizo mongoDB y mySQL .
         </Typography>
         <Typography variant="h3">Contacto</Typography>
         <Box mb={2} />
@@ -73,14 +91,16 @@ const AboutScreen = ({ changeTheme }) => {
             color: changeTheme ? "#E2E2E2" : "#050038",
             display: "flex",
             justifyContent: "flex-start",
+            cursor:"pointer"
           }}
+          onClick={()=> window.open('https://github.com/DiegoRodriguez-sc', '__blank')}
         >
           <Box sx={{ display: "flex" }}>
             <Box
               marginLeft={2}
               component="img"
               sx={{ width: "60px" }}
-              alt={"logo email"}
+              alt={"logo github"}
               src={github}
             />
             <Typography
@@ -103,14 +123,16 @@ const AboutScreen = ({ changeTheme }) => {
             color: changeTheme ? "#E2E2E2" : "#050038",
             display: "flex",
             justifyContent: "flex-start",
+            cursor:"pointer"
           }}
+          onClick={()=> window.open("https://www.linkedin.com/in/diego-rodriguez-sc/" , '__blank')}
         >
           <Box sx={{ display: "flex" }}>
             <Box
               marginLeft={2}
               component="img"
               sx={{ width: "60px" }}
-              alt={"logo email"}
+              alt={"logo linkedin"}
               src={linkedin}
             />
             <Typography
