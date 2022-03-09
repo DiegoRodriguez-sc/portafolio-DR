5b3c30b1d1c40aab2db70afc497b2e75286b69f1
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-03-08 22:41:22 -0300

    scroll stile change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,9 @@
 import React from "react";
 import WbSunnyIcon from "@mui/icons-material/WbSunny";
-import DarkModeIcon from '@mui/icons-material/DarkMode';
+import DarkModeIcon from "@mui/icons-material/DarkMode";
 import { Box, Container, Typography } from "@mui/material";
 
-const NavBar = ({setChangeTheme, changeTheme}) => {
-
+const NavBar = ({ setChangeTheme, changeTheme }) => {
   const mydate = new Date(),
     day = mydate.getDay();
 
@@ -21,14 +20,14 @@ const NavBar = ({setChangeTheme, changeTheme}) => {
   let time = dayArray[day] + " " + mydate.toLocaleTimeString().slice(0, -3);
 
   const handleTheme = () => {
-    if(changeTheme === "dark"){
+    if (changeTheme === "dark") {
       setChangeTheme("light");
       localStorage.setItem("theme", "light");
-    }else{
+    } else {
       setChangeTheme("dark");
       localStorage.setItem("theme", "dark");
     }
-  }
+  };
   return (
     <Container maxWidth="sm" sx={{ pt: 3 }}>
       <Box
@@ -37,11 +36,11 @@ const NavBar = ({setChangeTheme, changeTheme}) => {
         <Typography variant="h5">{time}</Typography>
 
         <Box component="div" sx={{ cursor: "pointer" }} onClick={handleTheme}>
-           {
-            changeTheme === "dark"
-              ? (<WbSunnyIcon style={{ fontSize: 25 }} />)
-              : (<DarkModeIcon style={{ fontSize: 25 }} />)
-           }
+          {changeTheme === "dark" ? (
+            <WbSunnyIcon style={{ fontSize: 25 }} />
+          ) : (
+            <DarkModeIcon style={{ fontSize: 25 }} />
+          )}
         </Box>
       </Box>
     </Container>
